e440a6de8bdcecba889d74564ec5c9e6a294052e
Ronnie Song <ronniesong0809@gmail.com>
2020-05-13 19:01:11 -0700

    feat: code cleaning, and use react-router

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        testing
+      </div>
+    );
+  }
+}
+
+export default Home;
