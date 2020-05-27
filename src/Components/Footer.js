0831bfe72813a6bc5ecd526a3a3628f83f5d6e95
Ronnie Song <ronniesong0809@gmail.com>
2020-05-27 02:35:35 -0700

    feat: refactor search bar and footer/add nav items and section tags/remove eslint/fix incorrect url

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import {Navbar, NavbarBrand} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap'
 
 class Footer extends Component {
   render() {
     return (
-      <Navbar fixed="bottom" bg="primary" variant="dark" className="justify-content-center">
-        <NavbarBrand>
-            &copy; 2020 by <a href="https://www.github.com/ronniesong0809/scanner-webapp" style={{color:"gray"}}>Ronnie Song</a>, All right reserved
-        </NavbarBrand>
+      <Navbar fixed="bottom" bg="dark" variant="dark" className="justify-content-center">
+        <span style={{color:"white"}}>
+            &copy; 2020 by <a href="https://www.github.com/ronniesong0809/dashboard">Ronnie Song</a>, All right reserved
+        </span>
       </Navbar>
     )
   }
