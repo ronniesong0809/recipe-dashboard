0e6bf87bc36ca36b53615acbcf1dff087d5398de
Ronnie Song <ronniesong0809@gmail.com>
2020-06-01 00:14:19 -0700

    feat: Dashboard Doughnut bar is now use real data from get request

--- src/Components/Chart/DashboardDoughnut.js
+++ src/Components/Chart/DashboardDoughnut.js
@@ -1,40 +1,31 @@
 import React, { Component } from 'react'
 import {Doughnut} from 'react-chartjs-2';
 
-const data = {
-	labels: [
-		'FAT',
-		'Protein',
-		'Carbs'
-	],
-	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-		'#ff6384',
-		'#b2ff9e',
-		'#ffce56'
-		],
-		hoverBackgroundColor: [
-		'#ffb0c0',
-		'#e1ffd9',
-		'#fff0cc'
-		]
-	}]
-};
-
 class DashboardDoughnut extends Component {
   render() {
+    let data = {
+      labels: [
+        'FAT',
+        'Protein',
+        'Carbs'
+      ],
+      datasets: [{
+        data: [
+          this.props.recipes[0].recipe.totalNutrients.CHOCDF.quantity,
+          this.props.recipes[0].recipe.totalNutrients.PROCNT.quantity,
+          this.props.recipes[0].recipe.totalNutrients.FAT.quantity
+        ],
+        backgroundColor: ['rgba(255,206,86,0.5)', 'rgba(178,255,158,0.5)', 'rgba(255,99,132,0.5)'],
+        borderColor: ['rgba(255,206,86,1)', 'rgba(178,255,158,1)', 'rgba(255,99,132,1)'],
+        borderWidth: 1,
+        hoverBackgroundColor: ['rgba(255,206,86,0.8)', 'rgba(178,255,158,0.8)', 'rgba(255,99,132,0.8)'],
+        hoverBorderColor: ['rgba(255,206,86,1)', 'rgba(178,255,158,1)', 'rgba(255,99,132,1)'],
+      }]
+    };
     return (
       <div>
-        <h5>Carb, Protein and Fat Breakdown</h5>
-        <Doughnut 
-            data={data}
-            options={{
-              legend: {
-                display:false
-              }
-            }}
-        />
+		    <h5>Carbs, Protein and Fat Breakdown</h5>
+        <Doughnut data={data} />
       </div>
     );
   }
